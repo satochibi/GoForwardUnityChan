1c02f1db6ba0ace6e31060ec8ac88cd8bbc64aef
satochibi <satochibi0120@gmail.com>
2017-07-05 12:34:11 +0900

    音を鳴らそうを実装

--- Assets/UnityChanController.cs
+++ Assets/UnityChanController.cs
@@ -41,6 +41,9 @@ public class UnityChanController : MonoBehaviour {
         bool isGround = (transform.position.y > this.groundLevel) ? false : true;
         this.animator.SetBool("isGround", isGround);
 
+        // ジャンプ状態のときにはボリュームを0にする
+        GetComponent<AudioSource>().volume = (isGround) ? 1 : 0;
+
         // 着地状態でクリックされた場合
         if (Input.GetMouseButtonDown(0) && isGround)
         {
